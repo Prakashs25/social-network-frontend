67bf5d790d1ebfa31954a4ea148c18e3de4159e4
PRAKASH S <prakashs25698@gmail.com>
2020-04-17 18:02:41 +0530

    Updates

---
+++ precache-manifest.df82e6c5315370f889a35d269e161379.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba7e30f2cf7149f7b1f6",
+    "url": "/social-network-frontend/static/css/main.1bc6e4eb.chunk.css"
+  },
+  {
+    "revision": "ba7e30f2cf7149f7b1f6",
+    "url": "/social-network-frontend/static/js/main.e1feef82.chunk.js"
+  },
+  {
+    "revision": "efc92760ad08eddbb372",
+    "url": "/social-network-frontend/static/js/runtime~main.486d0be7.js"
+  },
+  {
+    "revision": "77d403f54f6231a714c0",
+    "url": "/social-network-frontend/static/js/2.a20933e6.chunk.js"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/social-network-frontend/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "ca06346c3b547689ba80be850aae0194",
+    "url": "/social-network-frontend/static/media/iconHi.ca06346c.png"
+  },
+  {
+    "revision": "3e4bcd8804c9cd6cfe5fac227aff5818",
+    "url": "/social-network-frontend/index.html"
+  }
+];
